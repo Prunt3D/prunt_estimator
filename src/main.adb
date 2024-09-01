6e65240699de30e03b863ca64881750fe036e0da
Liam Powell <liam@liampwll.com>
2024-09-01 22:00:46 +0800

    Update for latest Prunt.

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,8 @@
 with Prunt;                use Prunt;
 with Prunt.Motion_Planner; use Prunt.Motion_Planner;
-with Prunt.Motion_Planner.Planner;
-with Prunt.Gcode_Parser;   use Prunt.Gcode_Parser;
 with Ada.Text_IO;          use Ada.Text_IO;
+with Prunt.Motion_Planner.Planner;
+with Prunt.Gcode_Parser;
 with Ada.Command_Line;
 with GNAT.OS_Lib;
 with Ada.Exceptions;
@@ -20,6 +20,14 @@ package body Main is
       return False;
    end Is_Homing_Move;
 
+   package My_Gcode_Parser is new Prunt.Gcode_Parser
+     (Stepper_Name      => Integer,
+      Heater_Name       => Integer,
+      Thermistor_Name   => Integer,
+      Fan_Name          => Integer,
+      Input_Switch_Name => Integer);
+   use My_Gcode_Parser;
+
    package My_Planner is new Motion_Planner.Planner
      (Flush_Extra_Data_Type        => Boolean,
       Flush_Extra_Data_Default     => False,
@@ -27,26 +35,24 @@ package body Main is
       Initial_Position             => [others => 0.0 * mm],
       Max_Corners                  => 3_000,
       Is_Homing_Move               => Is_Homing_Move,
+      Interpolation_Time           => 58_490.0 / 1_200_000_000.0 * s,
       Home_Move_Minimum_Coast_Time => 0.0 * s);
 
    Limits : constant Motion_Planner.Kinematic_Parameters :=
      (Lower_Pos_Limit         => (others => Length'First),
       Upper_Pos_Limit         => (others => Length'Last),
       Ignore_E_In_XYZE        => True,
-      Shift_Blended_Corners   => True,
-      Tangential_Velocity_Max => 10_000_000_000.0 * mm / s,
+      Shift_Blended_Corners   => False,
+      Tangential_Velocity_Max => 300.0 * mm / s,
       Axial_Velocity_Maxes    =>
-        (X_Axis => 100.0 * mm / s, Y_Axis => 100.0 * mm / s, Z_Axis => 50.0 * mm / s, E_Axis => 15.0 * mm / s),
+        (X_Axis => 300.0 * mm / s, Y_Axis => 300.0 * mm / s, Z_Axis => 300.0 * mm / s, E_Axis => 300.0 * mm / s),
       Pressure_Advance_Time   => 0.0 * s,
-      Acceleration_Max        => 50_000.0 * mm / s**2,
-      Jerk_Max                => 226_000.0 * mm / s**3,
-      --  Snap_Max                => 2_000_000.0 * mm / s**4,
-      --  Acceleration_Max        => 2_000.0 * mm / s**2,
-      --  Jerk_Max                => 1_000_000_000_000.0 * mm / s**3,
-      Snap_Max                => 1_000_000_000_000.0 * mm / s**4,
-      Crackle_Max             => 1_000_000_000_000.0 * mm / s**5,
+      Acceleration_Max        => 10_000.0 * mm / s**2,
+      Jerk_Max                => Jerk'Last,
+      Snap_Max                => Snap'Last,
+      Crackle_Max             => Crackle'Last,
       Chord_Error_Max         => 0.1 * mm,
-      Axial_Scaler            => (X_Axis => 1.0, Y_Axis => 1.0, Z_Axis => 0.2, E_Axis => 0.1));
+      Axial_Scaler            => (X_Axis => 1.0, Y_Axis => 1.0, Z_Axis => 1.0, E_Axis => 1.0));
 
    task Reader is
       entry Start (Filename : String);
@@ -54,8 +60,7 @@ package body Main is
 
    task body Reader is
       F              : File_Type;
-      C              : Gcode_Parser.Command;
-      Parser_Context : Gcode_Parser.Context := Make_Context ((others => 0.0 * mm), 100.0 * mm / s);
+      Parser_Context : My_Gcode_Parser.Context := Make_Context ((others => 0.0 * mm), 100.0 * mm / s);
    begin
       accept Start (Filename : String) do
          Open (F, In_File, Filename);
@@ -63,16 +68,20 @@ package body Main is
 
       while not End_Of_File (F) loop
          declare
-            Line : String := Get_Line (F);
+            Line : constant String := Get_Line (F);
+
+            procedure Runner (Comm : My_Gcode_Parser.Command) is
+            begin
+               if Comm.Kind = Move_Kind then
+                  My_Planner.Enqueue
+                    ((Kind              => My_Planner.Move_Kind,
+                      Pos               => Comm.Pos,
+                      Feedrate          => Comm.Feedrate,
+                      Corner_Extra_Data => (null record)));
+               end if;
+            end Runner;
          begin
-            Parse_Line (Parser_Context, Line, C);
-            if C.Kind = Move_Kind then
-               My_Planner.Enqueue
-                 ((Kind             => My_Planner.Move_Kind,
-                   Pos              => C.Pos,
-                   Feedrate         => C.Feedrate,
-                  Corner_Extra_Data => (null record)));
-            end if;
+            Parse_Line (Parser_Context, Line, Runner'Unrestricted_Access);
          exception
             when E : Bad_Line =>
                Put_Line ("Error on line:");
